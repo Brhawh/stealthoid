6780facdcafa2db6f4685df71e487a9db72b36c2
Brhawh <benn.henderson@instil.co>
2018-07-23 21:50:07 +0100

    Player object in scene with basic wasd movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		transform.Translate(x, y, 0);
+	}
+}
