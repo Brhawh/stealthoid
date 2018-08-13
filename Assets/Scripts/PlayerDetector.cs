c6bee5713889fee6c48a0a1e06f0a50366774cd1
Brhawh <benn.henderson@instil.co>
2018-08-13 15:28:23 +0100

    Removed unnecessary if checks, replaced with angle calculation. Pulled out some variables from FixedUpdate function.

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerDetector : MonoBehaviour {
 
+	private float rayAngle = 4;
+	private float rayDistance = 170;
+	private string playerObjectName = "Player";
+
 	void FixedUpdate() {
-		for (int i = 0; i < 3; i++) {
-			Vector2 direction = transform.up;
-			if (i == 1) {
-				direction = Quaternion.AngleAxis (4, Vector3.forward) * transform.up;
-			} else if (i == 2) {
-				direction = Quaternion.AngleAxis (-4, Vector3.forward) * transform.up;
-			}
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, direction, 170f);
+		for (int i = -1; i < 2; i++) {
+			Vector2 direction = Quaternion.AngleAxis (i * rayAngle, Vector3.forward) * transform.up;
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, direction, rayDistance);
+
 			if (hit.collider != null) {
 				Debug.DrawRay (transform.position, hit.point - new Vector2 (transform.position.x, transform.position.y), Color.green);	
-				if (hit.distance < 170f && hit.collider.gameObject.name == "Player") {
+				if (hit.collider.gameObject.name == playerObjectName) {
 					Debug.Log ("Player detected!");
 				}
 			} else {
-				Debug.DrawRay (transform.position, direction * 170f, Color.red);	
+				Debug.DrawRay (transform.position, direction * rayDistance, Color.red);	
 			}
 		}
 	}
