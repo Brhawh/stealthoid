e8d7c54f1fe2c13c1bc64da025b1d29c3eee8424
Brhawh <benn.henderson@instil.co>
2018-08-13 15:30:26 +0100

    Changed direction variable name to rayDirection.

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -10,8 +10,8 @@ public class PlayerDetector : MonoBehaviour {
 
 	void FixedUpdate() {
 		for (int i = -1; i < 2; i++) {
-			Vector2 direction = Quaternion.AngleAxis (i * rayAngle, Vector3.forward) * transform.up;
-			RaycastHit2D hit = Physics2D.Raycast (transform.position, direction, rayDistance);
+			Vector2 rayDirection = Quaternion.AngleAxis (i * rayAngle, Vector3.forward) * transform.up;
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, rayDirection, rayDistance);
 
 			if (hit.collider != null) {
 				Debug.DrawRay (transform.position, hit.point - new Vector2 (transform.position.x, transform.position.y), Color.green);	
@@ -19,7 +19,7 @@ public class PlayerDetector : MonoBehaviour {
 					Debug.Log ("Player detected!");
 				}
 			} else {
-				Debug.DrawRay (transform.position, direction * rayDistance, Color.red);	
+				Debug.DrawRay (transform.position, rayDirection * rayDistance, Color.red);	
 			}
 		}
 	}
