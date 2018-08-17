80393d5d437dca3881164f3004d8360ade9e9783
Benn Henderson <benn.henderson@instil.co>
2018-08-17 12:05:50 +0100

    Very basic AI where the enemy will patrol between the patrol points attached to the script.

---
+++ Assets/Scripts/EnemyPatrolAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolAI : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public float speed;
+
+	Transform currentPatrolPointTarget;
+	int currentPatrolPointTargetIndex;
+
+	// Use this for initialization
+	void Start () {
+		currentPatrolPointTarget = patrolPoints [0];
+		currentPatrolPointTargetIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up * Time.deltaTime * speed);
+
+		if (Vector3.Distance (transform.position, currentPatrolPointTarget.position) < 1f) {
+			if (currentPatrolPointTargetIndex + 1 < patrolPoints.Length) {
+				currentPatrolPointTargetIndex++;
+			} else {
+				currentPatrolPointTargetIndex = 0;
+			}
+			currentPatrolPointTarget = patrolPoints [currentPatrolPointTargetIndex];
+		}
+
+		Vector3 patrolPointDirection = currentPatrolPointTarget.position - transform.position;
+		float angle = Mathf.Atan2 (patrolPointDirection.y, patrolPointDirection.x) * Mathf.Rad2Deg - 90f;
+
+		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, q, 180f);
+	}
+}
