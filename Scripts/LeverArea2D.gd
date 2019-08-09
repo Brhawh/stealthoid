0cd59ca380498e3fad8c97ff7e6d2dca0653f181
Unknown <a_hamilton@live.co.uk>
2019-08-09 01:17:51 +0100

    fixed gate so you can add nodes to be changed in editor instead of hard coding values, tilemap functionality in godot is shit :)

--- Scripts/LeverArea2D.gd
+++ Scripts/LeverArea2D.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export(Array, NodePath) var gates
+
 func _on_LeverArea2D_body_entered(body):
 	if body.get_name() == "Character":
-		get_parent().get_node("TileMap").leverPulled(position);
+		get_parent().get_node("TileMap").leverPulled(position, gates);
--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -2,9 +2,9 @@ extends TileMap
 
 var _tileset = get_tileset()
     
-func leverPulled(var charPos):
-	var tile_pos = world_to_map(charPos)
-	set_cellv(tile_pos, _tileset.find_tile_by_name("leverPulled"))
-	set_cellv(Vector2(30, 8), _tileset.find_tile_by_name("path"))
-	set_cellv(Vector2(30, 7), _tileset.find_tile_by_name("path"))
-	
+func leverPulled(var charPos, var gates):
+	#TODO add if to check if change back
+	set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("leverPulled"))
+	for i in range(0, gates.size()):
+    	set_cellv(world_to_map(get_node(gates[i]).get_position()), _tileset.find_tile_by_name("path"))
+		
