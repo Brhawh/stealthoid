5000dbfffd79add154d19831b11682b95bdd2f41
Benn Henderson <bhenderson05@qub.ac.uk>
2019-08-23 17:02:49 +0100

    Rotator node no longer has list of angles, rotates towards its target when target isn't null. Will be set in the parent nodes script to control rotation.

--- Scripts/RotateToAngle.gd
+++ Scripts/RotateToAngle.gd
@@ -1,62 +1,52 @@
 extends Node
 
-export(Array, float) var rotationAngles
 export(float) var rotationSpeed
-export(bool) var repeat
-
-var currentTargetIndex = 0
-var targetAngle
+export(NodePath) var targetNodePath
+var targetRotation
+var nodeToRotate
 
 const DEGREES_IN_CIRCLE = 360
 
+func _ready():
+	var nodeToRotate = get_node(targetNodePath)
+
 func _physics_process(delta):
-	var parentAngle = get_parent().rotation_degrees
+	if nodeToRotate == null:
+		nodeToRotate = get_parent()
+	var currentRotation = nodeToRotate.rotation_degrees
 	
-	if currentTargetIndex != -1:
-		targetAngle = rotationAngles[currentTargetIndex]
-	
-	if targetAngle != null:
-		var clockwiseAngle = 0
-		var antiClockwiseAngle = 0
-		if parentAngle > targetAngle:
-			clockwiseAngle = DEGREES_IN_CIRCLE - parentAngle + targetAngle
-			antiClockwiseAngle = parentAngle - targetAngle
-		else:
-			clockwiseAngle = targetAngle - parentAngle
-			antiClockwiseAngle = DEGREES_IN_CIRCLE - targetAngle + parentAngle
-		var shouldRotateClockwise = true
-		if clockwiseAngle > antiClockwiseAngle:
-			shouldRotateClockwise = false
-		rotateToAngle(delta, targetAngle, shouldRotateClockwise)
+	if targetRotation != null:
+		rotateToAngle(delta, targetRotation, getShouldRotateClockwise(currentRotation))
+
+func getShouldRotateClockwise(currentRotation):
+	var clockwiseAngle = 0
+	var antiClockwiseAngle = 0
+	if currentRotation > targetRotation:
+		clockwiseAngle = DEGREES_IN_CIRCLE - currentRotation + targetRotation
+		antiClockwiseAngle = currentRotation - targetRotation
+	else:
+		clockwiseAngle = targetRotation - currentRotation
+		antiClockwiseAngle = DEGREES_IN_CIRCLE - targetRotation + currentRotation
+	return not(clockwiseAngle > antiClockwiseAngle)
 
 func rotateToAngle(delta, rotationTarget, shouldRotateClockwise):
 	var parentAngle = get_parent().rotation_degrees
 	var angleToRotate = rotationSpeed * delta
 	
-	parentAngle = updateCurrentAngle(parentAngle, angleToRotate, shouldRotateClockwise)
+	parentAngle = updateCurrentRotation(parentAngle, angleToRotate, shouldRotateClockwise)
 	
-	if abs(parentAngle + angleToRotate - rotationTarget) < angleToRotate:
-		updateTargetAngle(parentAngle, rotationTarget)
+	if abs(parentAngle + angleToRotate - rotationTarget) - 5 < angleToRotate:
+		targetRotation = null
 		
 	get_parent().rotation_degrees = parentAngle
 	
-func updateCurrentAngle(currentAngle, angleToRotate, shouldRotateClockwise):
+func updateCurrentRotation(currentRotation, angleToRotate, shouldRotateClockwise):
 	if shouldRotateClockwise:
-		currentAngle += angleToRotate
-		if currentAngle >= DEGREES_IN_CIRCLE:
-			currentAngle = fmod(currentAngle, DEGREES_IN_CIRCLE)
+		currentRotation += angleToRotate
+		if currentRotation >= DEGREES_IN_CIRCLE:
+			currentRotation = fmod(currentRotation, DEGREES_IN_CIRCLE)
 	else:
-		currentAngle -= angleToRotate
-		if currentAngle < 0:
-			currentAngle = DEGREES_IN_CIRCLE - fmod(abs(currentAngle), DEGREES_IN_CIRCLE)
-	return currentAngle
-
-func updateTargetAngle(parentAngle, rotationTarget):
-	parentAngle = rotationTarget
-	if currentTargetIndex < rotationAngles.size() - 1:
-		currentTargetIndex += 1
-	elif repeat:
-		currentTargetIndex = 0
-	else:
-		currentTargetIndex = -1
-		targetAngle = null
+		currentRotation -= angleToRotate
+		if currentRotation < 0:
+			currentRotation = DEGREES_IN_CIRCLE - fmod(abs(currentRotation), DEGREES_IN_CIRCLE)
+	return currentRotation
