2cca95320d772b31533696540770f5049d1cb8e0
Unknown <a_hamilton@live.co.uk>
2019-12-03 22:08:03 +0000

    fixed nesting issue with tilemap

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -7,8 +7,8 @@ func leverPulled(var charPos, var gates, var state):
 	if state == 0:
 		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("leverPulled"))
 		for i in range(0, gates.size()):
-    		set_cellv(world_to_map(get_node(gates[i]).get_position()), _tileset.find_tile_by_name("path"))
+    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("path"))
 	else:
 		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("leverNotPulled"))
 		for i in range(0, gates.size()):
-    		set_cellv(world_to_map(get_node(gates[i]).get_position()), _tileset.find_tile_by_name("gateVert"))
+    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("gateVert"))
