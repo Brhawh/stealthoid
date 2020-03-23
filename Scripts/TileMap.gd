706961793ebbf9e415b059347401a369c35345c8
Unknown <a_hamilton@live.co.uk>
2020-03-23 17:55:28 +0000

    Pyramid tilemap
    trying 16x16 so some assets are scaled down to 0.5x should be updated later. tweaked some scripts to adjust for new tile names

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -5,10 +5,10 @@ var _tileset = get_tileset()
 func leverPulled(var charPos, var gates, var state):
 	#TODO add if to check if change back
 	if state == 0:
-		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("leverPulled"))
+		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("LeverPulled.png"))
 		for i in range(0, gates.size()):
-    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("path"))
+    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("Floor.png"))
 	else:
-		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("leverNotPulled"))
+		set_cellv(world_to_map(charPos), _tileset.find_tile_by_name("LeverNotPulled.png"))
 		for i in range(0, gates.size()):
-    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("gateVert"))
+    		set_cellv(world_to_map(get_node("../" + gates[i]).get_position()), _tileset.find_tile_by_name("DoorClosed.png"))
--- scenes/Treasure.gd
+++ scenes/Treasure.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_Treasure_body_entered(body):
 	if body.get_name() == "Character":
-		$Sprite.visible = false
+		$TreasureSprite.visible = false
