f75622bcab009e6cb143bfac600a68c49e0faf5b
Brhawh <benn.r.henderson@gmail.com>
2020-10-21 20:48:18 +0100

    Added 3 more enemies to the lower right room of the pyrammidlevel1 scene. Had to connect the signal from the navigation2d node to each of them in the navigation2d script.

--- Scripts/Navigation2D.gd
+++ Scripts/Navigation2D.gd
@@ -4,4 +4,7 @@ signal navigationReady(navigation2DNode)
 
 func _ready():
 	connect("navigationReady", get_node("../Enemy"), "onNavigatorReady")
+	connect("navigationReady", get_node("../Enemy2"), "onNavigatorReady")
+	connect("navigationReady", get_node("../Enemy3"), "onNavigatorReady")
+	connect("navigationReady", get_node("../Enemy4"), "onNavigatorReady")
 	emit_signal("navigationReady", self)
