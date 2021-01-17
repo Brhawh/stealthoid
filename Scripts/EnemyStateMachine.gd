a98b79bdf763c478e426629203837959ce93b00a
Brhawh <benn.r.henderson@gmail.com>
2021-01-17 15:16:11 +0000

    Added state indicators that are set when the state gets changed. Default is Patrolling so if the initial state isn't patrolling it won't be correct until a change in state occurs.

--- Scripts/EnemyStateMachine.gd
+++ Scripts/EnemyStateMachine.gd
@@ -2,6 +2,15 @@ extends "res://Scripts/StateMachine.gd"
 
 class_name EnemyStateMachine
 
+# State Indicators
+var patrol_indicator = preload("res://Assets/patrolling_indicator.png")
+var attack_indicator = preload("res://Assets/attacking_indicator.png")
+var chase_indicator = preload("res://Assets/chasing_indicator.png")
+var guard_indicator = preload("res://Assets/guardin_indicator.png")
+var investigate_indicator = preload("res://Assets/investigating_indicator.png")
+
+onready var indicator_sprite = $"../StateIndicatorSprite"
+
 func handleTargetDetected(target):
 	if state.name != "Chasing" && state.name != "Attacking":
 		get_node("Chasing").target = target
@@ -15,3 +24,18 @@ func handleTargetLost():
 		state.handleTargetLost()
 	if state.name == "Attacking":
 		state.handleTargetLost()
+		
+func change_to(new_state):
+	.change_to(new_state)
+	
+	match new_state:
+		"Patrolling":
+			indicator_sprite.set_texture(patrol_indicator)
+		"Guarding":
+			indicator_sprite.set_texture(guard_indicator)
+		"Chasing":
+			indicator_sprite.set_texture(chase_indicator)
+		"Investigating":
+			indicator_sprite.set_texture(investigate_indicator)
+		"Attacking":
+			indicator_sprite.set_texture(attack_indicator)
